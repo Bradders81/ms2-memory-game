5d374ca63c233551fef040ef752aac1a169fc57b
Bradders81 <p-bradley@live.co.uk>
2021-03-27 20:37:01 +0000

    Add gamesTurn function

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,16 +8,15 @@ document.addEventListener('DOMContentLoaded', () => {
     let gameBtn = document.getElementsByClassName('gameButtons');
 
     for (buttons of gameBtn) {
-     buttonArray.push(buttons);
-}
-
-console.log(buttonArray)
+        buttonArray.push(buttons);
+    }
 
 
-    let gamesChoice = buttonArray[Math.floor(Math.random() * buttonArray.length)];
-        
- 
-   
+
+    function gamesTurn() {
+        let gamesChoice = buttonArray[Math.floor(Math.random() * buttonArray.length)]; //Credit: this line taken from https://www.kirupa.com/html5/picking_random_item_from_array.htm
+        gamePattern.push(gamesChoice);
+    }
 
 
 
