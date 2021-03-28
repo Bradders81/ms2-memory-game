8af7fd412413ce709dca519b644ec858e69b85d1
Bradders81 <p-bradley@live.co.uk>
2021-03-28 23:51:06 +0000

    Update game button style, remove counter span function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,19 +3,17 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     let startBtn = document.getElementById('start');
-    let userPattern = []; //Array to store the selections by the gamesTurn function
+    let userPattern = []; //Array to store the of selections by the gamesTurn function
     let gamePattern = []; //Array to sore the selections by the usersTurn function
     const buttonArray = []; //Array to hold games buttons for the game to choose from randomly
 
 
-    //gets the game buttons and pushes them into the buttonArray
+    //gets the game button id and pushes them into the buttonArray
     let gameBtn = document.getElementsByClassName('gameButtons');
     for (buttons of gameBtn) {
-        buttonArray.push(buttons);
+        let gameBtnID = buttons.id
+        buttonArray.push(gameBtnID);
     }
-
-
-
 
     //Plays sound when game buttons pressed/selected
     function sound(pressed) {
@@ -34,12 +32,19 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    function roundCounter(num) {
-        let roundDiv = document.getElementById('roundCounter');
-        let roundSpan = document.createElement('span');
-        roundSpan.id = 'round';
-        roundSpan.innerHTML= (num)
-        roundDiv.appendChild(roundSpan)
+    //     function buildRoundCounter(num) {
+    //         let roundDiv = document.getElementById('roundCounter');
+    //         roundSpan = document.createElement('span');
+    //         roundSpan.id = 'round';
+    //         roundSpan.innerHTML= (num)
+    //         roundDiv.appendChild(roundSpan)
+    //     }
+
+    // build
+
+
+    function countToRoundCounter() {
+
     }
 
     function startGame() {
@@ -49,49 +54,63 @@ document.addEventListener('DOMContentLoaded', () => {
         gamesTurn()
 
     }
+    gamesTurn()
 
     //updates the gamePattern array
     function gamesTurn() {
         let gamesChoice = buttonArray[Math.floor(Math.random() * buttonArray.length)]; //Credit: this line taken from https://www.kirupa.com/html5/picking_random_item_from_array.htm
-        gamePattern.push(gameChoice);
+        gamePattern.push(gamesChoice);
+
     }
 
+    console.log()
+
     // function displayGamesPattern()
+
 
     //Checks what the user clicked and updates userPattern array.  Also makes button clicked flash to user.
     for (buttonsClicked of gameBtn) {
         buttonsClicked.addEventListener("click", function () {
-            userPattern.push(buttonsClicked)
             clickedBtnID = this.id
+            userPattern.push(clickedBtnID)
             sound(clickedBtnID)
             this.style.backgroundColor = 'rgb(250, 250, 250)' //Shade of white
+            this.style.boxShadow = 'none';
             setTimeout(function () {
                 if (clickedBtnID === 'btn-green') {
                     changeBack = document.getElementById("btn-green")
                     changeBack.style.backgroundColor = 'rgb(19, 226, 36)'; //green
+                    changeBack.style.boxShadow = '#000 6px 4px 4px';
+
                 }
                 else if (clickedBtnID === 'btn-blue') {
                     changeBack = document.getElementById("btn-blue")
                     changeBack.style.backgroundColor = 'rgb(13, 13, 228)'; //blue
+                    changeBack.style.boxShadow = '#000 6px 4px 4px';
+
                 }
                 else if (clickedBtnID === 'btn-purple') {
                     changeBack = document.getElementById("btn-purple")
                     changeBack.style.backgroundColor = 'rgb(185, 22, 185)'; //purple
+                    changeBack.style.boxShadow = '#000 6px 4px 4px';
+
                 }
                 else if (clickedBtnID === 'btn-red') {
                     changeBack = document.getElementById("btn-red")
                     changeBack.style.backgroundColor = 'rgb(223, 18, 18)'; //red
+                    changeBack.style.boxShadow = '#000 6px 4px 4px';
                 }
                 else alert('Sorry something went wrong')
-               
+
+
             }, 300)
-             roundCounter(1)
+            console.log(userPattern)
 
         });
     }
 
 
- 
+
 
 
 
