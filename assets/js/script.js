b7d8c5c71258b0b1c06852b22e5f8ae77ca4664a
Bradders81 <p-bradley@live.co.uk>
2021-03-30 12:36:15 +0000

    Add setTimeout to displayPattern function to create btn flashes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,10 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
+
     let startBtn = document.getElementById('start');
-    let roundCounter = 0;
-    let score = 0;
+    var roundCounter = 1;
+
     let reset;
     let userPattern = []; //Array to store the of selections by the gamesTurn function
     let gamePattern = []; //Array to sore the selections by the usersTurn function
@@ -41,8 +42,6 @@ document.addEventListener('DOMContentLoaded', () => {
     startBtn.addEventListener('click', function () {
         userPattern = [];
         gamePattern = [];
-        roundCounter = 0;
-        score = 0;
         //PLACEHOLDER: play start game sound
         setTimeout(function () {
             gamesTurn();
@@ -53,48 +52,62 @@ document.addEventListener('DOMContentLoaded', () => {
     function gamesTurn() {
         let gamesChoice = buttonArray[Math.floor(Math.random() * buttonArray.length)]; //this method was learned from  https://www.kirupa.com/html5/picking_random_item_from_array.htm
         gamePattern.push(gamesChoice);
+        // roundCounter();
         displayGamesPattern();
     }
 
-//Displays the content of the gamePattern array each round to the user to simulate the games turn at pressing the game buttons
+    //Displays the content of the gamePattern array each round to the user to simulate the games turn at pressing the game buttons
     function displayGamesPattern() {
-        for (displayPattern of gamePattern) {
-            let = btnSound = displayPattern
-            sound(btnSound);
-            let BtnShow = document.getElementById(btnSound)
-            BtnShow.style.backgroundColor = 'rgb(250, 250, 250)' //Shade of white
-            BtnShow.style.boxShadow = 'none';
+        for (let i = 0; i < gamePattern.length; i++) {
             setTimeout(function () {
-                if (BtnShow.id === 'btn-green') {
-                    // changeBack = document.getElementById("btn-green")
-                    BtnShow.style.backgroundColor = 'rgb(19, 226, 36)'; //green
-                    BtnShow.style.boxShadow = '#000 6px 4px 4px';
-                }
-                else if (BtnShow.id === 'btn-blue') {
-                    // BtnShow= document.getElementById("btn-blue")
-                    BtnShow.style.backgroundColor = 'rgb(13, 13, 228)'; //blue
-                    BtnShow.style.boxShadow = '#000 6px 4px 4px';
-                }
-                else if (BtnShow.id === 'btn-purple') {
-                    // changeBack = document.getElementById("btn-purple")
-                    BtnShow.style.backgroundColor = 'rgb(185, 22, 185)'; //purple
-                    BtnShow.style.boxShadow = '#000 6px 4px 4px';
-                }
-                else if (BtnShow.id === 'btn-red') {
-                    // changeBack = document.getElementById("btn-red")
-                    BtnShow.style.backgroundColor = 'rgb(223, 18, 18)'; //red
-                    BtnShow.style.boxShadow = '#000 6px 4px 4px';
-                }
-                else alert('Sorry something went wrong')
-            }, 300)
+                btnSound = gamePattern[i];
+                BtnShow = document.getElementById(btnSound)
+                sound(btnSound);
+                BtnShow.style.backgroundColor = 'rgb(250, 250, 250)' //Shade of white
+                BtnShow.style.boxShadow = 'none'
+                setTimeout(function () {
+                    if (BtnShow.id === 'btn-green') {
+                        // changeBack = document.getElementById("btn-green")
+                        BtnShow.style.backgroundColor = 'rgb(19, 226, 36)'; //green
+                        BtnShow.style.boxShadow = '#000 6px 4px 4px';
+                    }
+                    else if (BtnShow.id === 'btn-blue') {
+                        // BtnShow= document.getElementById("btn-blue")
+                        BtnShow.style.backgroundColor = 'rgb(13, 13, 228)'; //blue
+                        BtnShow.style.boxShadow = '#000 6px 4px 4px';
+                    }
+                    else if (BtnShow.id === 'btn-purple') {
+                        // changeBack = document.getElementById("btn-purple")
+                        BtnShow.style.backgroundColor = 'rgb(185, 22, 185)'; //purple
+                        BtnShow.style.boxShadow = '#000 6px 4px 4px';
+                    }
+                    else if (BtnShow.id === 'btn-red') {
+                        // changeBack = document.getElementById("btn-red")
+                        BtnShow.style.backgroundColor = 'rgb(223, 18, 18)'; //red
+                        BtnShow.style.boxShadow = '#000 6px 4px 4px';
+                    }
+                    else alert('Sorry something went wrong')
+                }, 500)
+            }, i * 1000)
         }
+
+        console.log(gamePattern)
     }
 
+
+
+
+
+
+
+    //USERS TURN 
     //Checks what the user clicked and updates userPattern array.  Also makes button clicked flash to user.
     for (buttonsClicked of gameBtn) {
         buttonsClicked.addEventListener("click", function () {
-            clickedBtnID = this.id
+            let clickedBtnID = this.id
             userPattern.push(clickedBtnID)
+            let userGuessNum = userPattern
+            let gameGuessNum = gamePattern
             sound(clickedBtnID)
             this.style.backgroundColor = 'rgb(250, 250, 250)' //Shade of white
             this.style.boxShadow = 'none';
@@ -113,6 +126,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     changeBack = document.getElementById("btn-purple")
                     changeBack.style.backgroundColor = 'rgb(185, 22, 185)'; //purple
                     changeBack.style.boxShadow = '#000 6px 4px 4px';
+                    ;
                 }
                 else if (clickedBtnID === 'btn-red') {
                     changeBack = document.getElementById("btn-red")
@@ -121,10 +135,33 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
                 else alert('Sorry something went wrong')
             }, 300)
+            checkMatch(userGuessNum)
+            console.log(userGuessNum)
         });
     }
 
-   
+
+
+    console.log(userPattern)
+
+    function checkMatch(x, y) {
+        if (userPattern[x] === gamePattern[y] && userPattern.length === gamePattern.length) {
+            userPattern = []
+            setTimeout(function () {
+                gamesTurn()
+            }, 1000)
+        } else if (userPattern.length != gamePattern.length) {
+            setTimeout(function () {
+                return
+            }, 2000)
+        } else if (userPattern[x] == gamePattern[x]) {
+            return
+        } else {
+            alert('game over');
+        }
+    }
+
 });
+
 
 
