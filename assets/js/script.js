7004a669d39daac9f333649742e867e16717af9c
Bradders81 <p-bradley@live.co.uk>
2021-04-01 13:29:55 +0000

    Add disable clikc

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+    $('.gameButtons').attr("disabled", true)
     let startBtn = document.getElementById('start');
     let reset = document.getElementById('reset')
 
-    let userPattern = []; //Array to store the of selections by the gamesTurn function
-    let gamePattern = []; //Array to sore the selections by the usersTurn function
-    const buttonArray = []; //Array to hold games buttons for the game to choose from randomly
+    let userPattern = []; // Array to store the of selections by the gamesTurn function
+    let gamePattern = []; // Array to sore the selections by the usersTurn function
+    let buttonArray = []; // Array to hold games buttons for the game to choose from randomly
 
-    //gets all id of all the game button id and pushes them into the buttonArray
+    // Gets all id of all the game button id and pushes them into the buttonArray
     let gameBtn = document.getElementsByClassName('gameButtons');
     for (buttons of gameBtn) {
         let gameBtnID = buttons.id
         buttonArray.push(gameBtnID);
     }
-
-    //Plays sound when game buttons pressed/selected
+ 
+    // Plays sound when game buttons pressed/selected
     function sound(pressed) {
         if (pressed == 'btn-green') {
             beep1 = new Audio('assets/sounds/beep1.wav');
@@ -31,7 +31,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    //Credit: Influence for this function came from: Code Institute Love Math Project walk-through.
+    // Credit: Influence for this function came from: Code Institute Love Math Project walk-through.
     function countToRoundCounter() {
         let roundNum = gamePattern.length;
         document.getElementById("round").innerHTML = roundNum;
@@ -40,71 +40,75 @@ document.addEventListener('DOMContentLoaded', () => {
     startBtn.addEventListener('click', function () {
         userPattern = [];
         gamePattern = [];
-        //PLACEHOLDER: play start game sound
+
+        // PLACEHOLDER: play start game sound
         setTimeout(function () {
             gamesTurn()
         }, 1000);
     });
 
-    //updates the gamePattern array each round when it is the games turn
+    // Updates the gamePattern array each round when it is the games turn
     function gamesTurn() {
-        let gamesChoice = buttonArray[Math.floor(Math.random() * buttonArray.length)]; //Credit: method on this line learned from  https://www.kirupa.com/html5/picking_random_item_from_array.htm
+        let gamesChoice = buttonArray[Math.floor(Math.random() * buttonArray.length)]; // Credit: method on this line learned from:  https://www.kirupa.com/html5/picking_random_item_from_array.htm
         gamePattern.push(gamesChoice);
         countToRoundCounter()
         displayGamesPattern()
     }
 
-    //Displays the content of the gamePattern array each round to the user to simulate the games turn at pressing the game buttons
+    // Displays the content of the gamePattern array each round to the user to simulate the games turn at pressing the game buttons
     function displayGamesPattern() {
         for (let i = 0; i < gamePattern.length; i++) {
             setTimeout(function () {
                 btnSound = gamePattern[i];
                 BtnShow = document.getElementById(btnSound);
                 sound(btnSound);
-                BtnShow.style.backgroundColor = 'rgb(250, 250, 250)' //Shade of white
+                BtnShow.style.backgroundColor = 'rgb(250, 250, 250)' // Shade of white
                 BtnShow.style.boxShadow = 'none'
                 setTimeout(function () {
                     if (BtnShow.id === 'btn-green') {
-                        BtnShow.style.backgroundColor = 'rgb(19, 226, 36)'; //green
+                        BtnShow.style.backgroundColor = 'rgb(19, 226, 36)'; // green
                         BtnShow.style.boxShadow = '#000 6px 4px 4px';
                     }
                     else if (BtnShow.id === 'btn-blue') {
-                        BtnShow.style.backgroundColor = 'rgb(13, 13, 228)'; //blue
+                        BtnShow.style.backgroundColor = 'rgb(13, 13, 228)'; // blue
                         BtnShow.style.boxShadow = '#000 6px 4px 4px';
                     }
                     else if (BtnShow.id === 'btn-purple') {
-                        BtnShow.style.backgroundColor = 'rgb(185, 22, 185)'; //purple
+                        BtnShow.style.backgroundColor = 'rgb(185, 22, 185)'; // purple
                         BtnShow.style.boxShadow = '#000 6px 4px 4px';
                     }
                     else if (BtnShow.id === 'btn-red') {
-                        BtnShow.style.backgroundColor = 'rgb(223, 18, 18)'; //red
+                        BtnShow.style.backgroundColor = 'rgb(223, 18, 18)'; // red
                         BtnShow.style.boxShadow = '#000 6px 4px 4px';
                     }
                     else alert('Sorry something went wrong');
                 }, 500);
-            }, i * 600);
+                if (i == gamePattern.length - 1) {
+                    $('.gameButtons').attr("disabled", false); // enables button clicks
+                }
+                }, i * 600);
         }
     }
 
-    //USERS TURN 
-    //Checks what the user clicked and updates userPattern array.  Also makes button clicked flash to user.
+    // USERS TURN 
+    // Checks what the user clicked and updates userPattern array.  Also makes button clicked flash to user.
     for (buttonsClicked of gameBtn) {
         buttonsClicked.addEventListener("click", function () {
             let clickedBtnID = this.id;
             userPattern.push(clickedBtnID);
             let clickIndex = userPattern.length - 1;
             sound(clickedBtnID);
-            this.style.backgroundColor = 'rgb(250, 250, 250)' //Shade of white
+            this.style.backgroundColor = 'rgb(250, 250, 250)' // Shade of white
             this.style.boxShadow = 'none';
             setTimeout(function () {
                 if (clickedBtnID === 'btn-green') {
                     changeBack = document.getElementById("btn-green");
-                    changeBack.style.backgroundColor = 'rgb(19, 226, 36)'; //green
+                    changeBack.style.backgroundColor = 'rgb(19, 226, 36)'; // green
                     changeBack.style.boxShadow = '#000 6px 4px 4px';
                 }
                 else if (clickedBtnID === 'btn-blue') {
                     changeBack = document.getElementById("btn-blue");
-                    changeBack.style.backgroundColor = 'rgb(13, 13, 228)'; //blue
+                    changeBack.style.backgroundColor = 'rgb(13, 13, 228)'; // blue
                     changeBack.style.boxShadow = '#000 6px 4px 4px';
                 }
                 else if (clickedBtnID === 'btn-purple') {
@@ -122,7 +126,9 @@ document.addEventListener('DOMContentLoaded', () => {
             checkMatch(clickIndex)
         });
     }
-    //Checks the User's is submitting the correct pattern.
+
+
+    //Checks if the User is submitting the correct pattern.
     function checkMatch(clickIndex) {
         if (userPattern[clickIndex] === gamePattern[clickIndex]) {
             if (userPattern.length === gamePattern.length) {
@@ -132,29 +138,30 @@ document.addEventListener('DOMContentLoaded', () => {
                     score()
                 }, 1500);
             } else {
-                return
             }
         } else if (userPattern[clickIndex] !== gamePattern[clickIndex]) {
+
             alert(' That was not correct - GAME OVER!')
         }
     }
+
     //Resets the game for the user
     reset.addEventListener('click', function () {
         userPattern = []
         gamePattern = []
         document.getElementById("round").innerHTML = "0";
         document.getElementById("score").innerHTML = "0";
-        document.getElementById('hscore').innerHTML ="0"
+        document.getElementById('hscore').innerHTML = "0"
         // document.getElementById('score').innerHTML = ""
     })
 
     function flashAll() {
 
     }
-function score() {
-document.getElementById('hscore').innerHTML = gamePattern.length *10;
+    function score() {
+        document.getElementById('score').innerHTML = gamePattern.length * 10;
 
-}
+    }
 
     function EndOfGame() {
 
@@ -164,6 +171,14 @@ document.getElementById('hscore').innerHTML = gamePattern.length *10;
 
     }
 
-});
+    // function turnOn() {
+    //     $('.gameButtons').attr("disabled", false);
+    // }
 
+    // function turnOff() {
+    //     $('.gameButtons').attr("disabled", true);
+    // }
+
+
+});
 
